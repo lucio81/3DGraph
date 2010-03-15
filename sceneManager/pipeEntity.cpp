b70ba6a569caa2a86faff0f05e59fc1534ec58b1
lucio81 <berardi.luciano@gmail.com>
2010-03-16 00:00:07 +0100

    Added rotatePipe method, this allow to rotate a pipe in the direction of 2 given points

--- sceneManager/pipeEntity.cpp
+++ sceneManager/pipeEntity.cpp
@@ -1,5 +1,10 @@
 #include <qmath.h>
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
 #include"sceneManager/pipeEntity.h"
+
+#define PI 3.14159265
 
 
 static inline QVector<QVector3D> extrude(const QVector<QVector3D> &verts, qreal depth)
@@ -45,4 +50,61 @@ PipeEntity::PipeEntity(Geometry *g, qreal iRad, qreal oRad, qreal depth, int num
         os->addQuad(out_back[(i + 1) % numSectors], out_back[i],
                     outside[i], outside[(i + 1) % numSectors]);
     parts << front << back << is << os;
+}
+
+void PipeEntity::rotatePipe(QVector3D node1, QVector3D node2){
+
+    //calculating the angle on a plan passing for 2 points
+
+    //Plan xy
+
+    double radians1;
+    double degreesXY;
+    if(node2.y()-node1.y() == 0){
+        degreesXY = 0.0;
+    }
+    else{
+        radians1 = atan((node2.x()-node1.x())/(node2.y()-node1.y()));
+        degreesXY= radians1 *  180 / PI;
+    }
+    std::cout.precision(2);
+    std::cout << "asse X:" << degreesXY << std::endl;
+
+
+
+    //Plan zy
+    double radians2;
+    double degreesYZ;
+    if(node2.z()-node1.z() == 0){
+        degreesYZ= 0.0;
+    }
+    else{
+
+        radians2 = atan((node2.y()-node1.y())/(node2.z()-node1.z()));
+        degreesYZ = radians2 *  180 / PI;
+    }
+    std::cout.precision(2);
+    std::cout << "asse Y:" << degreesYZ << std::endl;
+
+    //Plan zx
+    double radians3;
+    double degreesZX;
+    if(node2.x()-node1.x() == 0){
+        degreesZX= 0.0;
+    }
+    else{
+
+        radians3 = atan((node2.z()-node1.z())/(node2.x()-node1.x()));
+        degreesZX = radians3 *  180 / PI;
+    }
+    std::cout.precision(2);
+    std::cout << "asse Z:" << degreesZX << std::endl;
+
+
+
+
+    rotate(degreesXY,QVector3D(1.0,0.0,0.0));
+    rotate(degreesYZ,QVector3D(0.0,1.0,0.0));
+    rotate(degreesZX,QVector3D(0.0,0.0,1.0));
+
 }
--- sceneManager/pipeEntity.h
+++ sceneManager/pipeEntity.h
@@ -2,11 +2,13 @@
 #define PIPEENTITY_H
 
 #include<sceneManager/netEntity.h>
+#include<sceneManager/nodeEntity.h>
 
 class PipeEntity : public NetEntity{
 
 public:
     PipeEntity(Geometry *g, qreal iRad, qreal oRad, qreal depth, int numSectors);
+    void rotatePipe(QVector3D node1, QVector3D node2);
 
 };
 
